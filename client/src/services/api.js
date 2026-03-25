8cae44882b399916b0c7c6c258d238a142ddd109
keshavlabs <kkarwa04@gmail.com>
2026-03-25 17:59:04 +0530

    Connection to CORS

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-powered-finance-tracker-1-babv.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
