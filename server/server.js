940da1bc4f0fb799d67d442c96fcf2697193292b
keshavlabs <kkarwa04@gmail.com>
2026-03-25 12:52:27 +0530

    Connected frontend to deployed backend and fixed CORS

--- server/server.js
+++ server/server.js
@@ -12,7 +12,12 @@ const aiRoutes = require("./routes/ai.routes");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://ai-powered-finance-tracker-1-babv.onrender.com",
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
 app.use("/api/budgets", budgetRoutes);
